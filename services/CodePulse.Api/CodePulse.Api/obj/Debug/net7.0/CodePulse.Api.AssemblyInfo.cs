6a99d87e7f788d5a75cbf9b2ea181f6e8899d43e
Hrushikesh Phapale <hrushikesh.phapale@hloov.com>
2024-07-05 08:55:30 +0400

    initial

--- services/CodePulse.Api/CodePulse.Api/obj/Debug/net7.0/CodePulse.Api.AssemblyInfo.cs
+++ services/CodePulse.Api/CodePulse.Api/obj/Debug/net7.0/CodePulse.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca37b09e02605ef56fec9b9cb585dbdf13d4ffdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
